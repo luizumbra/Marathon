89d34f69e0e08e5371e328066a620a5861371628
luizumbra <luiz_fantasma@hotmail.com>
2016-02-23 20:14:36 -0300

    Em ./2014/minhoca.c foi eleborado e testado as variáveis que capturam as variáveis.

--- 2014/minhoca.c
+++ 2014/minhoca.c
@@ -16,24 +16,58 @@ typedef struct consulta {
   int m;
 }Consulta;
 
+void mostrarTunel(Tunel*, int);
+void mostrarConsulta(Consulta*, int);
+
 int main(int argc, char **argv) {
-  int i;
+  int i, j;
 
   // Preparando a entrada
-  int s, t;
+  int s, t, q;
+
   // Número de salões
   s = atoi(argv[1]);
+  // printf("%d", s);
+
   // Número de túneis
   t = atoi(argv[2]);
-  // Descrição do Túnel
-  Tunel desc[t];
-  for(i = 0; i < t; i++) {
-    desc[i].a = atoi(argv[i * 3 + 3]);
-    desc[i].b = atoi(argv[i * 3 + 4]);
-    desc[i].c = atoi(argv[i * 3 + 5]);
-  }
-  // Número de consultas
-  int q;
+  // printf("%d\n", t);
 
+  // Descrição do Túnel
+  Tunel descT[t];
+  for(i = 0; i < t; i++) {
+    descT[i].a = atoi(argv[i * 3 + 3]);
+    descT[i].b = atoi(argv[i * 3 + 4]);
+    descT[i].c = atoi(argv[i * 3 + 5]);
+  }
+  // mostrarTunel(descT, t);
+
+  // Número de consultas
+  i = (i - 1) * 3 + 5 + 1;
+  q = atoi(argv[i]);
+  // printf("%d\n", q);
+  
+  // Descrição da Consulta
+  Consulta descC[q];
+  for(j = 0; j < q; j++) {
+    descC[j].x = atoi(argv[j * 2 + i + 1]);
+    descC[j].m = atoi(argv[j * 2 + i + 2]);
+  }
+  // mostrarConsulta(descC, q);
+  
   return 0;
+}
+
+void mostrarTunel(Tunel *t, int m) {
+  int i;
+  for(i = 0; i < m; i++) {
+    printf("%d %d %d\n", t[i].a, t[i].b, t[i].c);
+  }
+}
+
+void mostrarConsulta(Consulta *c, int t) {
+  int i;
+  for(i = 0; i < t; i++) {
+    printf("%d %d\n", c[i].x, c[i].m);
+  }
 }
